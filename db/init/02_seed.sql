d383eab557f21c06268a2172c8bf720a51de76ac
Corentin Hautefaye <the.r3fraction@gmail.com>
2026-03-11 17:21:34 +0100

    THE CHOSEN ONE

--- db/init/02_seed.sql
+++ db/init/02_seed.sql
@@ -1,9 +1,1 @@
-USE test_db;
-
-INSERT INTO users (name, email) VALUES
-                                    ('Alice', 'alice@example.com'),
-                                    ('Bob', 'bob@example.com');
-
-INSERT INTO products (name, price) VALUES
-                                       ('Widget A', 9.99),
-                                       ('Widget B', 19.99);
+/*USE wonka_db;*/
