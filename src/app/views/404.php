d63a9578cd39797050777eafdb22aa7137c738e5
Geemiinii <theorigal39@gmail.com>
2026-04-08 11:12:17 +0200

    further work on 404, home
    adjusted default path to home.php, added header.php and navbar.php to
    put on all pages for improved navigation

--- src/app/views/404.php
+++ src/app/views/404.php
@@ -1,3 +1,4 @@
+<?php include 'partials/header.php'; ?>
 <div class="container">
     <div class="error-page">
         <h1>404</h1>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,9 @@
 <?php
+return [
+    'GET' => [
+        '/' => ['HomeController', 'home'],
+    ],
+    'POST' => [
+
+    ],
+];
