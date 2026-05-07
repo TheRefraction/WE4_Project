a257b6ca47226eb544cd4d73eb0735dfc538448c
Corentin Hautefaye <the.r3fraction@gmail.com>
2026-05-07 16:10:03 +0200

    Smol commit

--- db/migrations/V1_1_0__populate_product_category.sql
+++ db/migrations/V1_1_0__populate_product_category.sql
@@ -4,4 +4,4 @@ INSERT INTO product_category(name)
 VALUES ('Viande'), ('Crudité'), ('Légume'), ('Pain'), ('Fromage'), ('Sauce'), ('Autre'), 
 ('Boisson'), ('Bière'), ('Vin'), ('Boisson chaude'), 
 ('Végétarien'), ('Sans gluten'), ('Sans lactose'),
-('Accompagnement'), ('Salade'), ('Burger'), ('Pizza'), ('Tacos'), ('Sandwich'), ('Panino'), ('Box'), ('Assiette'), ('Dessert');
+('Accompagnement'), ('Salade'), ('Burger'), ('Pizza'), ('Tacos'), ('Sandwich'), ('Panino'), ('Dessert');
--- db/migrations/V1_1_3_populate_product.sql
+++ db/migrations/V1_1_3_populate_product.sql
@@ -23,8 +23,18 @@ VALUES ('Steak', NULL, 0.0, NULL, TRUE),
 ('Algérienne', NULL, 0.0, 1, TRUE),
 ('Barbecue', NULL, 0.0, 1, TRUE),
 
-('Koufola', NULL, 0.0, 1, FALSE),
-('Bière', NULL, 0.0, 1, FALSE),
-('Vin', NULL, 0.0, 2, FALSE),
-('Café', NULL, 0.0, NULL, FALSE),
-('Thé', NULL, 0.0, NULL, FALSE);
+('Tacos', 'Un tacos simple', 5.0, NULL, FALSE),
+('Tacos 2 viandes', 'Un tacos à deux viandes', 6.0, NULL, FALSE),
+('Double Tacos', 'Un grand tacos à deux viandes', 10.0, NULL, FALSE),
+('Burger', 'Un burger merveilleux (1, 2 ou 3 steaks)', 2.5, NULL, FALSE),
+('Döner', 'Döner', 5.0, NULL, FALSE),
+('Frites', 'Des frites', 2.0, NULL, FALSE),
+('Pizza simple', 'Pizza', 6.0, NULL, FALSE),
+
+(''),
+
+('Koufola', 'Un soda exceptionnel', 0.0, 1, FALSE),
+('Bière', 'Blonde 50cL', 0.0, 1, FALSE),
+('Vin', 'Du vin écoplus', 0.0, 2, FALSE),
+('Café', 'Il caffé della Mamma', 0.0, NULL, FALSE),
+('Thé', 'Bon thé turc', 0.0, NULL, FALSE);
