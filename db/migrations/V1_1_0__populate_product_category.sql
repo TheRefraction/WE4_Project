2d74df74735a1a2c6d82d653bb2cb0e7275eaf74
Corentin Hautefaye <the.r3fraction@gmail.com>
2026-05-09 00:06:21 +0200

    DB-chan has been fed

--- db/migrations/V1_1_0__populate_product_category.sql
+++ db/migrations/V1_1_0__populate_product_category.sql
@@ -1,7 +1,18 @@
 -- V1_1_0__populate_product_category.sql
 
-INSERT INTO product_category(name) 
-VALUES ('Viande'), ('Crudité'), ('Légume'), ('Pain'), ('Fromage'), ('Sauce'), ('Autre'), 
-('Boisson'), ('Bière'), ('Vin'), ('Boisson chaude'), 
-('Végétarien'), ('Sans gluten'), ('Sans lactose'),
-('Accompagnement'), ('Salade'), ('Burger'), ('Pizza'), ('Tacos'), ('Sandwich'), ('Panino'), ('Dessert');
+INSERT INTO product_category(id, name) 
+VALUES 
+(1, 'Viande'), 
+(2, 'Crudité'), 
+(3, 'Pain'), 
+(4, 'Sauce'), 
+(5, 'Autre'), 
+(6, 'Boisson'), 
+(7, 'Bière'), 
+(8, 'Vin'), 
+(9, 'Boisson chaude'), 
+(10, 'Accompagnement'), 
+(11, 'Burger'), 
+(12, 'Pizza'), 
+(13, 'Tacos'), 
+(14, 'Dessert');
