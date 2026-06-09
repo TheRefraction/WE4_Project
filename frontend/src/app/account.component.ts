58e4796d34742abc6bceab35f24779285541ea0e
Kenneth SOARES <kensoares@gmail.com>
2026-06-09 20:04:23 +0200

    add a product card with customisable items

--- frontend/src/app/account.component.ts
+++ frontend/src/app/account.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
-export class AccountComponent {}
+export class AccountComponent {
+
+  accountForm = signal({
+    first_name: 'Jean',
+    last_name: 'Dupont',
+    email: 'jean.dupont@gmail.com',
+    phone:'+33 6 12 34 56 78'
+  });
+
+  passwordForm = signal({
+    new_password: '',
+    confirm_new_password: '',
+    actual_password: ''
+  });
+
+  onUpdateAccount() {
+    // this.http.post('/route', this.accountForm);
+  }
+
+  onUpdatePassword() {
+    // this.http.post('/route', this.accountForm);
+  }
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AccountComponent } from './account.component';
 import { HomeComponent } from './home.component';
+import { ShopComponent } from './shop.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'account', component: AccountComponent}
+  { path: 'account', component: AccountComponent},
+  { path: 'shop', component: ShopComponent }
 ];
